90e259f05ab5da1ab00a99680cdbc4d7cf633960
elienemas <elienemas@gmail.com>
2022-02-08 21:33:26 -0300

    exercicio sala de aula

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #modulo2
+teste
---
+++ chat.js
@@ -0,0 +1,33 @@
+// const input  = require("readline-sync")
+// // esta entrada permite que a variável input utilize a readline-sync. O comando require é usado antes da dependência
+
+// console.log("Seja bem vindo(a)! Vamos fazer seu login")
+
+// const idade = input.question ("Insira sua idade:") 
+ 
+
+// if (idade >= 16 && idade <18) {
+//     console.log('pode entrar, mas não pode beber')
+//   } else if (idade >=18) {
+//       console.log("Pode entrar e bora beber")
+//   }  else    {
+//     console.log('entrada permitida apenas para maiores de idade.')
+//   }
+
+// PROJETO PISTA DE AUTORAMA
+
+const input = require("readline-sync");
+
+console.log("Seja bem vindo(a)! Vamos fazer seu login")
+
+const idade = input.question("Insira sua idade:") 
+const carteira = input.question("Voce tem habilitacao? Digite Sim ou Nao: ") 
+
+
+if (idade>= 18 && carteira === "sim" ) {
+         console.log("pode entrar, e pode dirigir")
+} else  {
+    console.log("Pode entrar, mas nao pode dirigir")
+};
+         
+//

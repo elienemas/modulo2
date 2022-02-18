7df24240e6a5fcdaf4e37574061fd88c1d885702
elienemas@gmail.com <elienemas@gmail.com>
2022-02-17 21:33:45 -0300

    ex aula

---
+++ Dados.js
@@ -0,0 +1,10 @@
+console.log(`Hello`+ ' ' + "World")
+console.log("Hello "+"world")
+
+myname =  "Maria"
+console.log( "Hello" + ' '+ myname
+)
+
+
+
+
--- calc.js
+++ calc.js
@@ -2,12 +2,16 @@ const input = require("readline-sync");
 
 console.log("Seja bem vindo(a) Ao Auto Cart! Vamos fazer seu login")
 
-var n1 = parseFloat("Insira nota 1")
-var n2 = parseFloat("Insira nota 2")
-var n3 = parseFloat("Insiat nota 3")
-var n4 = parseFloat("Insiat nota 4")
-var n5 = parseFloat("Insiat nota 5")
+var n1 = ("Insira nota 1")
+var n2 = ("Insira nota 2")
+var n3 = ("Insiat nota 3")
+var n4 = ("Insiat nota 4")
+var n5 = ("Insiat nota 5")
 
 var media = (n1+n2+n3+n4+n5) /5
 
 console.log(`sua média é ${media}`)
+if (media <=){
+
+    console.log ("reprovado")
+}else if (media >=4.1 && media <+6.9)

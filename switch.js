d451919532b0e14414ae504f6117fdf050a25b0e
elienemas <97566728+elienemas@users.noreply.github.com>
2022-02-24 21:53:14 -0300

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,28 @@
+let dia = "Terca";
+
+switch (dia){
+    case 'Segunda':
+        console.log("Uhuu! Começou a semana");
+    break
+    case 'Terca':    
+    case 'Quarta':
+        console.log("Aguenta ai e pega teu café, a semana não acabou");
+    break
+    case 'Quinta':
+        console.log("É quase sexta, mas não Sextou");
+    break
+    case 'Sexta':
+        console.log("Sextouuuuuu");
+    case 'Sábado':
+    case 'Domingo':
+        console.log("Chegou o final de semana, bom descanso");
+        break
+        default:
+            console.log("Não achei sua condição");
+}
+
+
+
+
+
+
---
+++ switch1.js
@@ -0,0 +1,17 @@
+let fruta = "abacate";
+
+switch (fruta) {
+    case 'laranja':
+        console.log("5.60kg");
+    break;
+    case 'abacate':
+        console.log("1.99un");
+    break;
+    case 'maçã':
+        console.log("10.00kg");
+        break;
+    default:
+        console.log("Produto não cadastrado");
+        
+}
+
